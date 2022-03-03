cfb2e030cbed13f4881fa9a9979a8be899e548a8
PauBadiaM <pau.badia@uni-heidelberg.de>
2022-03-03 09:32:29 +0100

    Updated annotation

--- scripts/pipeline/annotate.sh
+++ scripts/pipeline/annotate.sh
@@ -12,8 +12,8 @@ python scripts/plot/integrate_metrics.py -i data/prc/sc/annotated.h5ad -v 1 -o f
 python scripts/process/annotate.py -i data/prc/sc/annotated.h5ad -r 0.5 -o data/prc/sc/
 python scripts/plot/annotate_metrics.py -i data/prc/sc/annotated.h5ad -m data/markers.csv -v 1 -o figures
 
-# Remove sample specific cluster and Atros from Grey matter
-python scripts/process/remerge.py -i data/prc/sc/annotated.h5ad -c 10,16 -o data/prc/sc/
+# Remove sample specific clusters and Atros from Grey matter
+python scripts/process/remerge.py -i data/prc/sc/annotated.h5ad -c 10,15,16 -o data/prc/sc/
 python scripts/plot/merge_metrics.py -i data/prc/sc/annotated.h5ad -v 2 -o figures/
 
 python scripts/process/reintegrate.py -i data/prc/sc/annotated.h5ad -o data/prc/sc/
@@ -23,11 +23,25 @@ python scripts/process/annotate.py -i data/prc/sc/annotated.h5ad -r 0.5 -o data/
 python scripts/plot/annotate_metrics.py -i data/prc/sc/annotated.h5ad -m data/markers.csv -v 2 -o figures
 
 # Remove possible Astro-Oligo doublets
-python scripts/process/remerge.py -i data/prc/sc/annotated.h5ad -c 14 -o data/prc/sc/
+python scripts/process/remerge.py -i data/prc/sc/annotated.h5ad -c 13,14 -o data/prc/sc/
 python scripts/plot/merge_metrics.py -i data/prc/sc/annotated.h5ad -v 3 -o figures/
 
 python scripts/process/reintegrate.py -i data/prc/sc/annotated.h5ad -o data/prc/sc/
 python scripts/plot/integrate_metrics.py -i data/prc/sc/annotated.h5ad -v 3 -o figures/
 
-python scripts/process/annotate.py -i data/prc/sc/annotated.h5ad -r 0.5 -o data/prc/sc/
+python scripts/process/annotate.py -i data/prc/sc/annotated.h5ad -r 0.75 -o data/prc/sc/
 python scripts/plot/annotate_metrics.py -i data/prc/sc/annotated.h5ad -m data/markers.csv -v 3 -o figures
+
+# Remove possible Micro-Oligo doublets
+python scripts/process/remerge.py -i data/prc/sc/annotated.h5ad -c 13 -o data/prc/sc/
+python scripts/plot/merge_metrics.py -i data/prc/sc/annotated.h5ad -v 4 -o figures/
+
+python scripts/process/reintegrate.py -i data/prc/sc/annotated.h5ad -o data/prc/sc/
+python scripts/plot/integrate_metrics.py -i data/prc/sc/annotated.h5ad -v 4 -o figures/
+
+python scripts/process/annotate.py -i data/prc/sc/annotated.h5ad -r 0.75 -o data/prc/sc/
+python scripts/plot/annotate_metrics.py -i data/prc/sc/annotated.h5ad -m data/markers.csv -v 4 -o figures
+
+# Final Annotation
+python scripts/process/annotate.py -i data/prc/sc/annotated.h5ad -r 0.75 -o data/prc/sc/ -d 0:Oligos,1:Astros,2:Oligos,3:Oligos,4:Oligos,5:Microglia,6:Oligos,7:OPC,8:Endothelia,9:T-cells,10:Astros_r,11:Astros_c,12:B-cells,13:Macrophages_f,14:Stroma,15:Oligos_d,16:Astros
+python scripts/plot/annotate_metrics.py -i data/prc/sc/annotated.h5ad -m data/markers.csv -v 5 -o figures
--- scripts/plot/annotate_metrics.py
+++ scripts/plot/annotate_metrics.py
@@ -33,8 +33,8 @@ markers = pd.read_csv(markers_path)
 markers = dict(markers.groupby('cell_type')['gene'].apply(list))
 
 # Plot clusters and marker genes
-fig, ax = plt.subplots(1,2, figsize=(10,5), facecolor='white', tight_layout=True, dpi=150)
-sc.pl.umap(adata, color='leiden', ax=ax[0], return_fig=False, show=False, add_outline=False, s=10)
+fig, ax = plt.subplots(1,2, figsize=(14,7), facecolor='white', tight_layout=True, dpi=150, gridspec_kw={'width_ratios': [1, 1.5]})
+sc.pl.umap(adata, color='leiden', ax=ax[0], return_fig=False, show=False, add_outline=False, s=10, legend_loc='on data')
 sc.pl.dotplot(adata, markers, 'leiden', dendrogram=False, ax=ax[1], return_fig=False, show=False)
 
 # Save

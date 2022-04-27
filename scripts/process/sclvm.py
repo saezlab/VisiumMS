d96e9ee34941730b6c75a0af1b2404f14bd403d2
Pau Badia i Mompel <44896790+PauBadiaM@users.noreply.github.com>
2022-04-27 10:02:29 +0200

    Refactored sclvm model

--- scripts/process/sclvm.py
+++ scripts/process/sclvm.py
@@ -2,7 +2,7 @@ import scanpy as sc
 import numpy as np
 
 import os
-from scvi.model import CondSCVI, DestVI
+from scvi.model import CondSCVI
 
 import argparse
 
@@ -13,6 +13,7 @@ parser.add_argument('-r', '--path_raw', help='Path to raw single nuclei adata', 
 parser.add_argument('-s', '--path_slides', help='Path to raw visium slides', required=True)
 parser.add_argument('-n', '--label_name', help='Label of cell type from raw data', required=True)
 parser.add_argument('-d', '--sample_id', help='Label of sample id from raw data', required=True)
+parser.add_argument('-h', '--n_hvg', help='Number of variable genes to use', required=False, default=2000)
 parser.add_argument('-o', '--path_output', help='Path were to save model', required=True)
 args = vars(parser.parse_args())
 
@@ -21,9 +22,10 @@ path_slides = args['path_slides']
 label_name = args['label_name']
 sample_id = args['sample_id']
 path_output = args['path_output']
+n_hvg = args['n_hvg']
 
 # Read raw data
-adata = sc.read_h5ad(input_path)
+adata = sc.read_h5ad(path_raw)
 
 # Filter empty genes
 sc.pp.filter_genes(adata, min_counts=10)
@@ -34,18 +36,18 @@ adata.layers["counts"] = adata.X.copy()
 # Filter HVG genes
 sc.pp.normalize_total(adata)
 sc.pp.log1p(adata)
-sc.pp.highly_variable_genes(adata, batch_key='batch')
+sc.pp.highly_variable_genes(adata, batch_key=sample_id)
 adata.var = adata.var[['highly_variable','highly_variable_nbatches']]
 adata.raw = adata
 batch_msk = np.array(adata.var.highly_variable_nbatches > 1)
-hvg = adata.var[batch_msk].sort_values('highly_variable_nbatches').tail(num_hvg_genes).index
+hvg = adata.var[batch_msk].sort_values('highly_variable_nbatches').tail(n_hvg).index
 adata.var['highly_variable'] = [g in hvg for g in adata.var.index]
 adata.var = adata.var[['highly_variable','highly_variable_nbatches']]
 adata = adata[:,hvg]
 
 # Read Visium slides
 slides = []
-for dname in os.listdir(data_path):
+for dname in os.listdir(path_slides):
     if not dname.startswith('.'):
         tmp = sc.read_visium(os.path.join(path_slides, dname,'outs'))
         tmp.var_names_make_unique()
@@ -58,7 +60,7 @@ intersect = np.intersect1d(slides.var_names, adata.var_names)
 adata = adata[:, intersect].copy()
 
 # Set up scLVM model
-CondSCVI.setup_anndata(adata, layer="counts", labels_key=label_name, batch_key=sample_id)
+CondSCVI.setup_anndata(adata, layer="counts", labels_key=label_name)
 model = CondSCVI(adata, weight_obs=False)
 model.view_anndata_setup()
 
@@ -66,11 +68,5 @@ model.view_anndata_setup()
 model.train()
 
 # Save model
-model.save(path_output, overwrite=True)
-history = model.history["elbo_train"]
-
-
-
-
-
-
+model.save(os.path.join(path_output, 'sclvm.pt'), overwrite=True)
+model.history["elbo_train"].to_csv(os.path.join(path_output, 'sclvm_history.csv'))

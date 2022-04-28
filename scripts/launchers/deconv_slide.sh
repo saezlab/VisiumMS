ec73ceb1dfbbf3602fac4d41f8d92069e6869bf6
PauBadiaM <pau.badia@uni-heidelberg.de>
2022-04-28 10:53:41 +0200

    Added deconv launcher

---
+++ scripts/launchers/deconv_slide.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -p gpu
+#SBATCH --gres=gpu:1
+#SBATCH --time=03:00:00
+#SBATCH --chdir=/net/data.isilon/ag-saez/bq_pbadia/VisiumMS/
+
+
+# Deconvolute a single slide
+/net/data.isilon/ag-saez/bq_pbadia/programs/conda/envs/ms/bin/python scripts/process/stlvm.py -m $path_to_regres -s $path_to_slide -o $path_to_output

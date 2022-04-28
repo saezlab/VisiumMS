1816233bdbbb5cd0016751a023d1e70f4d0f994c
PauBadiaM <pau.badia@uni-heidelberg.de>
2022-04-28 14:42:51 +0200

    Updated get props to get both props and f_props

--- scripts/process/get_props.py
+++ scripts/process/get_props.py
@@ -1,16 +1,35 @@
 import scanpy as sc
 import numpy as np
 
-import destvi_utils
-
-from scvi.model import CondSCVI, DestVI
+from scvi.model import DestVI
 import destvi_utils
 
 import os
 import argparse
 
+parser = argparse.ArgumentParser(prog='get_prop', description='Gets proportions from deconv slide')
+parser.add_argument('-s', '--slide_path', help='Directory containing deconvoluted slide', required=True)
+args = vars(parser.parse_args())
 
-st_model = DestVI.load('data/prc/visium/MS377T/')
+slide_path = args['slide_path']
+
+# Load slide
+st_model = DestVI.load(slide_path)
+
+# Get props
 st_model.adata.obsm["proportions"] = st_model.get_proportions()
-thrs = destvi_utils.automatic_proportion_threshold(st_model.adata, kind_threshold='secondary', output_file='data/prc/visium/MS377T/thr.html')
+
+# Compute optimal props thresholds
+thrs = destvi_utils.automatic_proportion_threshold(st_model.adata, kind_threshold='secondary', output_file=os.path.join(slide_path, 'thr.html'))
+
+# Filter and recalculate props
+st_model.adata.obsm["f_proportions"] = st_model.adata.obsm["proportions"].copy()
+for cell_type in thrs.keys():
+    thr = thrs[cell_type]
+    vals = st_model.adata.obsm["f_proportions"][cell_type]
+    st_model.adata.obsm["f_proportions"][cell_type] = vals * (vals > thr)
+st_model.adata.obsm["f_proportions"] = st_model.adata.obsm["f_proportions"] / st_model.adata.obsm["f_proportions"].sum(1).values.reshape(-1,1)
+
+# Save
+st_model.adata.write(os.path.join(slide_path, 'adata.h5ad'))
 

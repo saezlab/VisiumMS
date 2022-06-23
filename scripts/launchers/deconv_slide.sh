ed50a0ce7c96d4ec83b2ded9c93b48261e97108a
PauBadiaM <pau.badia@uni-heidelberg.de>
2022-06-23 17:19:44 +0200

    Fixed basename bug in pipeline deconv

--- scripts/launchers/deconv_slide.sh
+++ scripts/launchers/deconv_slide.sh
@@ -6,5 +6,5 @@
 #SBATCH --exclude=cln146g
 
 # Deconvolute a single slide
-sample_id=basename $path_to_slide
+sample_id=$(basename $path_to_slide)
 /net/data.isilon/ag-saez/bq_pbadia/programs/conda/envs/ms/bin/python scripts/process/deconv.py -r $path_to_regres -s $path_to_slide -o $path_to_output/$sample_id

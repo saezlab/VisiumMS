4a5bec96f738fef48c7a0687ba7b3e561c59dfd4
PauBadiaM <pau.badia@uni-heidelberg.de>
2022-06-23 14:36:01 +0200

    Fixed bug deconv

--- scripts/launchers/deconv_slide.sh
+++ scripts/launchers/deconv_slide.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #SBATCH -p gpu
 #SBATCH --gres=gpu:1
-#SBATCH --time=03:00:00
+#SBATCH --time=06:00:00
 #SBATCH --chdir=/net/data.isilon/ag-saez/bq_pbadia/VisiumMS/
-#SBATCH --nodelist=denbi1,denbi2
+#SBATCH --exclude=cln146g
 
 # Deconvolute a single slide
-/net/data.isilon/ag-saez/bq_pbadia/programs/conda/envs/ms/bin/python scripts/process/deconv.py -r $path_to_regres -s $path_to_slide -o $path_to_output
+sample_id=basename $path_to_slide
+/net/data.isilon/ag-saez/bq_pbadia/programs/conda/envs/ms/bin/python scripts/process/deconv.py -r $path_to_regres -s $path_to_slide -o $path_to_output/$sample_id

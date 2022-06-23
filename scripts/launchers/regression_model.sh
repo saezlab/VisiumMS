196f5c88ea3ca2e908112c75c5c949e7f182dc5b
PauBadiaM <pau.badia@uni-heidelberg.de>
2022-06-23 11:18:25 +0200

    Moved regression model to sbatch script

--- scripts/launchers/regression_model.sh
+++ scripts/launchers/regression_model.sh
@@ -1,4 +1,12 @@
+#!/bin/bash
+#SBATCH -p gpu
+#SBATCH --gres=gpu:1
+#SBATCH --time=03:00:00
+#SBATCH --chdir=/net/data.isilon/ag-saez/bq_pbadia/VisiumMS/
+#SBATCH --nodelist=denbi1,denbi2
+
 echo '# Generating raw object from samples #'
 /net/data.isilon/ag-saez/bq_pbadia/programs/conda/envs/ms/bin/python scripts/process/get_raw.py -s data/raw/sc/ -a data/prc/sc/annotated.h5ad -o data/prc/sc/raw.h5ad
 echo '# Training regression model #'
 /net/data.isilon/ag-saez/bq_pbadia/programs/conda/envs/ms/bin/python scripts/process/regression_model.py -r data/prc/sc/raw.h5ad -n leiden -s sample_id -p 1 -o data/prc/visium/
+

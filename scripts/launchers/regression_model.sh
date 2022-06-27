9856ec6170eff701895ea13df5f0d85ad3b224f7
PauBadiaM <pau.badia@uni-heidelberg.de>
2022-06-27 18:05:31 +0200

    Fixed regression model launcher

--- scripts/launchers/regression_model.sh
+++ scripts/launchers/regression_model.sh
@@ -3,7 +3,9 @@
 #SBATCH --gres=gpu:1
 #SBATCH --time=03:00:00
 #SBATCH --chdir=/net/data.isilon/ag-saez/bq_pbadia/VisiumMS/
-#SBATCH --nodelist=denbi1,denbi2
+#SBATCH --exclude="cln146g"
+#SBATCH -J regr_m
+#SBATCH -o "logs/regression_model.out"
 
 echo '# Generating raw object from samples #'
 /net/data.isilon/ag-saez/bq_pbadia/programs/conda/envs/ms/bin/python scripts/process/get_raw.py -s data/raw/sc/ -a data/prc/sc/annotated.h5ad -o data/prc/sc/raw.h5ad

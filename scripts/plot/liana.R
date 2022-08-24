0c3ef815cccdc17e8a5946e0dd4609131b7c8734
PauBadiaM <pau.badia@uni-heidelberg.de>
2022-08-24 13:47:43 +0200

    Added liana

---
+++ scripts/plot/liana.R
@@ -0,0 +1,32 @@
+library(liana)
+library(tidyverse)
+
+
+plot_liana <- function(lesion_type, n = 5){
+    # Read liana results
+    lesion_type <- stringr::str_replace_all(lesion_type, " ", "")
+    res_path <- 'data/prc/liana'
+    res <- read.csv(sprintf("%s/%s.csv", res_path, lesion_type))
+
+    # Select top interacitons per cluster
+    top_inters <- res %>%
+        group_by(source, target) %>%
+        slice_min(aggregate_rank, n=5) %>%
+        ungroup() %>%
+        distinct_at(c('ligand.complex', 'receptor.complex'))
+
+    # Plot
+    plt <- liana_dotplot(inner_join(top_inters, res)) + 
+        theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+    # Save
+    plot_path <- 'figures/liana'
+    dir.create(plot_path, showWarnings = FALSE, recursive = TRUE)
+    pdf(file = sprintf('%s/%s.pdf', plot_path, lesion_type), width = 12, height = 12)
+    plot(plt)
+    dev.off()
+}
+
+plot_liana('Chronic Active', n=5)
+plot_liana('Control', n=5)
+
---
+++ scripts/process/liana.R
@@ -0,0 +1,54 @@
+library(zellkonverter)
+library(liana)
+library(tidyverse)
+
+
+# Read adata
+adata <- readH5AD('data/prc/sc/raw.h5ad')
+inters <- read.csv('data/prc/pca_misty/inters.csv')
+
+# Filter by lesion types
+msk <-  adata@colData$lesion_type %in% c('Chronic Active', 'Control')
+adata <- adata[, msk]
+
+# Extract unique cell types
+cell_types <- unique(unname(unlist(inters)))
+cell_types <- gsub('\\.', '_', cell_types)
+
+# Filter by cell types
+msk <-  adata@colData$leiden %in% cell_types
+adata <- adata[, msk]
+
+# Rename assays
+names(adata@assays) <- c("counts")
+
+# Normalize
+adata <- scuttle::logNormCounts(adata)
+
+# Assign label
+SingleCellExperiment::colLabels(adata) <- adata@colData$leiden
+
+
+run_liana <- function(adata, lesion_type){
+
+    # Filter by lesion_type
+    msk <- adata@colData$lesion_type == lesion_type
+    tmp <- adata[, msk]
+
+    # Run liana
+    res <- liana_aggregate(liana_wrap(tmp)) %>%
+        filter(aggregate_rank < 0.05) %>%
+        select(source, target, ligand.complex, receptor.complex,
+               aggregate_rank, natmi.edge_specificity, sca.LRscore)
+
+    # Write
+    lesion_type <- stringr::str_replace_all(lesion_type, " ", "")
+    res_path <- 'data/prc/liana/'
+    dir.create(res_path, showWarnings = FALSE, recursive = TRUE)
+    write.csv(res, sprintf('%s/%s.csv', res_path, lesion_type), row.names = FALSE)
+}
+
+
+run_liana(adata, 'Chronic Active')
+run_liana(adata, 'Control')
+

c8fd36671aea92bc568a12b75ce67a6b434ad9fd
schae211 <schae211@msu.edu>
2023-06-03 14:03:31 +0200

    vs

--- scripts/process/qc.py
+++ scripts/process/qc.py
@@ -1,3 +1,7 @@
+
+# python scripts/process/qc.py --output cellbender
+# python scripts/process/qc.py --output cellranger
+
 import scanpy as sc
 import scanpy.external as sce
 
@@ -6,48 +10,50 @@ import pandas as pd
 
 import pickle
 import os
+from pathlib import Path
 import argparse
-import urllib.request
 
-"""
-Script to run basic QC filtering. Stores data for future plotting and writes new AnnData objects.
-"""
+# add command line flag arguments to specify either "cellbender" or "cellranger" output
+parser = argparse.ArgumentParser()
+parser.add_argument("--output", type=str, required=True)
+args = parser.parse_args()
 
-# Read command line and set args
-parser = argparse.ArgumentParser(prog='qc', description='Run QC per sample')
-parser.add_argument('-i', '--input_dir', help='Input directory containing all sample directories', required=True)
-parser.add_argument('-o', '--output_dir', help='Output directory where to store the processed objects', required=True)
-args = vars(parser.parse_args())
+# set up relative paths within the project
+current_folder = Path(__file__).parent
+if args.output == "cellbender":
+    input_dir = current_folder / ".." / ".." / "data" / "prc" / "sc" / "cellbender"
+    output_dir = current_folder / ".." / ".." / "data" / "prc" / "sc" / "cellbender_qc"
+    mtx_name = "cell_bender_matrix_filtered.h5"
+elif args.output == "cellranger":
+    input_dir = current_folder / ".." / ".." / "data" / "raw" / "sc"
+    output_dir = current_folder / ".." / ".." / "data" / "prc" / "sc" / "cellranger_qc"
+    mtx_name = "filtered_feature_bc_matrix.h5"
+else:
+    raise ValueError("output must be either 'cellbender' or 'cellranger'")
+output_dir.mkdir(parents=True, exist_ok=True)
 
-base_path = args['input_dir']
-output_path = args['output_dir']
-###############################
+# verbose
+print("input_dir: ", input_dir)
+print("output_dir: ", output_dir)
+print("mtx_name: ", mtx_name)
 
 # Load diss genes
-diss_fname = 'coregene_df-FALSE-v3.csv'
-diss_path = os.path.join(base_path, diss_fname)
-
-if not os.path.isfile(diss_path):
-    url = 'https://raw.githubusercontent.com/kieranrcampbell/' \
-    'scrnaseq-digestion-paper/master/data/deliverables/coregene_df-FALSE-v3.csv'
-    urllib.request.urlretrieve(url, diss_path)
-    
+diss_path = "https://raw.githubusercontent.com/kieranrcampbell/scrnaseq-digestion-paper/master/data/deliverables/coregene_df-FALSE-v3.csv"
 diss_genes = pd.read_csv(diss_path).sort_values('PValue').head(200).gene_symbol.tolist()
-os.remove(diss_path)
 
+samples = [sample for sample in os.listdir(input_dir) if not sample.startswith(".")]
+
+# TODO: Not yet implemented this
 # If needed set up specific thresholds for doublet detection
 # The value should divide the bimodal distirbution in two
 doublet_thresholds = {
 }
 
 # Perform QC for each sample independently
-for sample in os.listdir(base_path):
-    if sample.startswith('.'):
-        continue
+for sample in samples:
     print(sample)
     # Read raw data
-    adata = sc.read_10x_mtx(os.path.join(base_path, sample, 'filtered_feature_bc_matrix'),
-                            var_names='gene_symbols', cache=True)
+    adata = sc.read_10x_h5(input_dir / sample / mtx_name)
     adata.var_names_make_unique()
 
     # Basic filtering
@@ -76,10 +82,7 @@ for sample in os.listdir(base_path):
     gene_qnt = 0.99
     diss_qnt = 0.99
 
-    if sample in doublet_thresholds:
-        doublet_thr = doublet_thresholds[sample]
-    else:
-        doublet_thr = 0.2
+    doublet_thr = doublet_thresholds[sample] if sample in doublet_thresholds else 0.2
 
     # Save cell meta data
     df = adata.obs[['n_genes_by_counts', 'total_counts', 'pct_counts_mt', 'doublet_score', 'diss_score']]
@@ -100,6 +103,5 @@ for sample in os.listdir(base_path):
     adata = adata[msk, :]
 
     # Save results
-    os.makedirs(os.path.join(output_path, sample), exist_ok=True)
-    pickle.dump(plot_data, open(os.path.join(output_path, sample, sample+'.pkl'), "wb"))
-    adata.write(os.path.join(output_path, sample,sample+'.h5ad'))
+    pickle.dump(plot_data, open(output_dir / f"{sample}.pkl", "wb"))
+    adata.write(output_dir / (sample + ".h5ad"))

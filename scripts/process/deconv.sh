24822ae2551e34153e3ab37d6476f3b40e1747ea
Philipp SL Schaefer <philipp.schaefer@stud.uni-heidelberg.de>
2023-06-07 17:50:51 +0200

    fix bug in deconv pipeline

--- scripts/process/deconv.sh
+++ scripts/process/deconv.sh
@@ -4,4 +4,4 @@ source /home/bq_pschaefer/.bashrc
 
 conda activate ms_env_1
 
-python /net/data.isilon/ag-saez/bq_pschaefer/VisiumMS_old/scripts/process/cell_bender_c2l.py --model $REG_MODEL --sample $SAMPLE --n_cells_spot $N_CELLS_SPOT --d_alpha $D_ALPHA --recompute $RECOMPUTE
+python /net/data.isilon/ag-saez/bq_pschaefer/VisiumMS_old/scripts/process/cell_bender_c2l.py --output $OUTPUT --model $REG_MODEL --sample $SAMPLE --n_cells_spot $N_CELLS_SPOT --d_alpha $D_ALPHA --recompute $RECOMPUTE

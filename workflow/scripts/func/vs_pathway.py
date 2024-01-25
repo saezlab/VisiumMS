80447e8866d727e4e8c3316d669bd89f969ba832
PauBadiaM <pau.badia@uni-heidelberg.de>
2024-01-25 20:06:08 +0100

    PW now takes into account space

--- workflow/scripts/func/vs_pathway.py
+++ workflow/scripts/func/vs_pathway.py
@@ -2,6 +2,7 @@ import scanpy as sc
 import pandas as pd
 import numpy as np
 import decoupler as dc
+import liana as li
 import os
 import argparse
 
@@ -21,7 +22,22 @@ out_path = args['out_path']
 # Read data
 slide = sc.read_h5ad(slide_path)
 
-# Read reactome and filter by sign
+def spatial_para(adata):
+    from scipy.sparse import csr_matrix
+    # Smooth by connectivity
+    X = adata.X
+    if isinstance(X, csr_matrix):
+        X = X.A
+    conn = adata.obsp['spatial_connectivities']
+    X = (conn @ X) / conn.sum(axis=1).A  # Normalize by spatial weights
+    
+    return X
+
+# Spatially weight
+li.ut.spatial_neighbors(slide, bandwidth=150, cutoff=0.1, kernel='gaussian', set_diag=True)
+slide.X = spatial_para(slide)
+
+# Read gene set database
 if db_name != 'progeny':
     gmt = dc.read_gmt(gmt_path)
     gmt['source'] = [s.split(db_name)[1].replace('_', ' ').lstrip() for s in gmt['source']]
@@ -29,6 +45,9 @@ if db_name != 'progeny':
 else:
     gmt = pd.read_csv(gmt_path).groupby('source', observed=True).head(1000)
     weight = 'weight'
+msk = ~gmt['source'].str.contains('FETAL|INFECTION|SARS', case=False)
+gmt = gmt[msk]
+
 
 # Run enrichment analysis
 dc.run_ulm(slide, gmt, weight=weight, use_raw=False)

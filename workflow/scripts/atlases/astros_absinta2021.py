84092dd480f8c82f2199d355265df95c52dbe95e
PauBadiaM <pau.badia@uni-heidelberg.de>
2024-06-04 17:27:32 +0200

    Added significance to absinta comparison for asc

--- workflow/scripts/atlases/astros_absinta2021.py
+++ workflow/scripts/atlases/astros_absinta2021.py
@@ -5,6 +5,7 @@ import scanpy as sc
 import matplotlib.pyplot as plt
 import matplotlib.backends.backend_pdf
 import seaborn as sns
+import scipy
 import argparse
 
 # Init args
@@ -39,19 +40,35 @@ groups_b = deg_ler['group'].unique()
 df = []
 for group_a in groups_a:
     for group_b in groups_b:
-        genes_a = deg_abs[deg_abs['group'] == group_a].set_index('names')
-        genes_b = deg_ler[deg_ler['group'] == group_b].set_index('names')
+        genes_a = deg_abs[deg_abs['group'] == group_a].set_index('names').head(100)
+        genes_b = deg_ler[deg_ler['group'] == group_b].set_index('names').head(100)
         inter = np.intersect1d(genes_a.index, genes_b.index)
+        diff_ab = np.setdiff1d(genes_a.index, genes_b.index)
+        diff_ba = np.setdiff1d(genes_b.index, genes_a.index)
         union = np.union1d(genes_a.index, genes_b.index)
         jacc = inter.size / union.size
-        df.append([group_a, group_b, inter.size, union.size, jacc])
-df = pd.DataFrame(df, columns=['group_a', 'group_b', 'inter', 'union', 'jacc'])
-df = df.pivot(index='group_a', columns='group_b', values='jacc')
+        odds, pval = scipy.stats.fisher_exact([[inter.size, diff_ab.size], [diff_ba.size, 20000]], alternative='greater')
+        df.append([group_a, group_b, inter.size, union.size, jacc, odds, pval])
+df = pd.DataFrame(df, columns=['group_a', 'group_b', 'inter', 'union', 'jacc', 'odds', 'pval'])
+df['padj'] = scipy.stats.false_discovery_control(df['pval'], method='bh')
+jacc = df.pivot(index='group_a', columns='group_b', values='jacc')
+order = jacc.max(0).sort_values(ascending=False).index
+jacc = jacc.loc[:, order]
+
+sign = pd.DataFrame(index=jacc.index, columns=jacc.columns)
+sign.loc[:, :] = ''
+
+for row in df[df['padj'] < 0.01][['group_a', 'group_b']].iterrows():
+    i, row = row
+    group_a, group_b = row['group_a'], row['group_b']
+    sign.loc[group_a, group_b] = '*'
+
+sign = sign.loc[:, order]
 
 # Plot
-order = df.max(0).sort_values(ascending=False).index
+
 fig1, ax = plt.subplots(1, 1, figsize=(6, 3), dpi=150)
-sns.heatmap(df.loc[:, order], cmap='Blues', square=True, cbar_kws={"shrink": 0.25, 'aspect': 5, 'label': 'Jaccard index'}, ax=ax)
+sns.heatmap(jacc, cmap='Blues', square=True, cbar_kws={"shrink": 0.25, 'aspect': 5, 'label': 'Jaccard index'}, ax=ax, annot=sign, fmt='')
 ax.set_xlabel('Lerma et al.')
 ax.set_ylabel('Absinta et al.')
 
@@ -73,7 +90,7 @@ def plot_violin(adata, gene, ax, palette):
     sns.violinplot(data=df, x='group', y='value', hue='group', ax=ax, palette=palette)
     ax.set_ylabel(gene)
     ax.set_xlabel('')
-    ax.get_legend().remove()
+    #ax.get_legend().remove()
 
 # Plot top marker genes
 fig2, axes = plt.subplots(2, 3, figsize=(5, 3), tight_layout=True, dpi=150)

75a4fbe10bfb3780e157574dd6e4dfe0a9698e8e
PauBadiaM <pau.badia@uni-heidelberg.de>
2024-06-13 11:32:39 +0200

    Updated volcano plots

--- workflow/scripts/func/summary_deg.py
+++ workflow/scripts/func/summary_deg.py
@@ -32,6 +32,27 @@ deg.loc[msk, 'contrast'] = 'CIvsCA'
 ctypes = ['OL', 'AS', 'MG']
 contrasts = ['CAvsCtrl', 'CIvsCtrl', 'CIvsCA']
 
+gdict = {
+    'AS': np.array([
+    'PDK1','ACSF2','TANGO2','AQP1','ARL17B','SMAD6','HMGB1','HLA-F-AS1', 'LRP1', 'PRKG2','OSMR',
+    'SERPINA3','NFE2L2', 'APP', 'C3', 'DST','TNFRSF1A', 'EEA1','DOCK11','RNF7','CCND2',
+    'CNDP2','HSPBP1', 'FOXJ1','ANXA1','SLITRK2','ITGA2','SCRG1','CLU','ITGB1','CFAP97',
+    'EBLN3P', 'COL6A1','CTSD','IFITM3','TAGLN','AKT3','CPNE3','KCNK2','PIK3IP1',
+    'IFITM2', 'ARL5B', 'NNT', 'CUL4B', 'ALDH1L1']),
+    'MG': np.array([
+    'PRAM1','ARHGAP12','TMEM119','HAMP','P2RY12',
+    'HMOX1','SIRPA','SPP1','TRAF3','PARP9','FLT1','PARVG','MS4A6A','CD14','PPARG','COPB2','C1QB',
+    'APOC1','FTL','C1QC','GPNMB','APP','AHI1','FPR3','CD163','CTSS','IQGAP1',
+    'PLXNC1','APOE','CD68','ITGA4','ANXA2','LGALS1','TGFBI','MS4A7','ITGB1',
+    'PIK3CG','PDCD10','AGPS','LRRK2','CLEC12A','CLEC7A','FRMD4A','CX3CR1', 'FOXP2']),
+    'OL': np.array([
+    'CAMK2A','ERBB2','CD226','PGM1','SOX13','ADAMTS4','PSEN1','DOCK3','NDE1','EPHB1',
+    'CDK18','AXIN1','SRA1','HSPB1','IRF1','EIF5','NFKB2','CALM1','THAP1','HSP90B1',
+    'CAMK2D','USP1','NGFR','ATF4','CD274','SLC22A17','EIF2S1','DCC','BRCA2',
+    'ITGB1','SOX4','LGALS3','OSMR','ANXA2','MYRIP','MPZ','GMFB','TGFBR2','VWA8','CD2AP',
+    'ARNTL','ELOVL6','SVEP1','RORA','SCD']),
+}
+
 def to_int(x):
     return int(np.round(x * 255))
 
@@ -39,6 +60,54 @@ def to_hex(row):
     r, g, b, a = row
     hex = '#%x%x%x' % (to_int(r), to_int(g), to_int(b))
     return hex
+
+def plot_volcano_df(data, x, y, top=5, sign_thr=0.05, lFCs_thr=0.5, sign_limit=None, lFCs_limit=None, color_pos='#D62728',
+                    color_neg='#1F77B4', color_null='gray', figsize=(7, 5), dpi=100, ax=None, return_fig=False, save=None):
+
+    # Load plotting packages
+    import adjustText as at
+
+    # Transform sign_thr
+    sign_thr = -np.log10(sign_thr)
+
+    # Extract df
+    df = data.copy()
+    df['logFCs'] = df[x]
+    df['pvals'] = -np.log10(df[y])
+
+    # Define color by up or down regulation and significance
+    df['weight'] = color_null
+    up_msk = (df['logFCs'] >= lFCs_thr) & (df['pvals'] >= sign_thr)
+    dw_msk = (df['logFCs'] <= -lFCs_thr) & (df['pvals'] >= sign_thr)
+    df.loc[up_msk, 'weight'] = color_pos
+    df.loc[dw_msk, 'weight'] = color_neg
+
+    # Plot
+    fig = None
+    if ax is None:
+        fig, ax = plt.subplots(1, 1, figsize=figsize, dpi=dpi)
+    df.plot.scatter(x='logFCs', y='pvals', c='weight', sharex=False, ax=ax)
+    ax.set_axisbelow(True)
+
+    # Draw sign lines
+    ax.axhline(y=sign_thr, linestyle='--', color="black")
+    ax.axvline(x=lFCs_thr, linestyle='--', color="black")
+    ax.axvline(x=-lFCs_thr, linestyle='--', color="black")
+
+    # Plot top sign features
+    signs = df[up_msk | dw_msk].sort_values('pvals', ascending=False)
+    signs = signs.loc[top[np.isin(top, signs.index.values.astype('U'))]]
+
+    # Add labels
+    ax.set_ylabel('-log10(pvals)')
+    texts = []
+    for x, y, s in zip(signs['logFCs'], signs['pvals'], signs.index):
+        texts.append(ax.text(x, y, s))
+    if len(texts) > 0:
+        at.adjust_text(texts, arrowprops=dict(arrowstyle='-', color='black'), ax=ax)
+
+    if return_fig:
+        return fig
 
 sn_figs = []
 for contrast in contrasts:
@@ -52,12 +121,13 @@ for contrast in contrasts:
         '#d62728': up_color,
     }
     for ctype in ctypes:
+        top = gdict[ctype]
         fig, ax = plt.subplots(1, 1, figsize=(3, 3), dpi=150)
-        dc.plot_volcano_df(
+        plot_volcano_df(
             data=deg[(deg['cell_type'] == ctype) & (deg['contrast'] == contrast)],
             x='log2FoldChange',
             y='pvalue',
-            top=10,
+            top=top,
             sign_thr=0.05,
             lFCs_thr=1,
             ax=ax,
@@ -97,7 +167,10 @@ deg = pd.read_csv(ns_deg_path, index_col=0)
 
 ctypes = ['PPWM', 'LR', 'LC', 'VI']
 contrasts = ['CIvsCA']
-
+top = []
+for k in gdict:
+    top.extend(list(gdict[k]))
+top = np.unique(top)
 ns_figs = []
 for contrast in contrasts:
     # Set color
@@ -111,11 +184,11 @@ for contrast in contrasts:
     }
     for ctype in ctypes:
         fig, ax = plt.subplots(1, 1, figsize=(3, 3), dpi=150)
-        dc.plot_volcano_df(
+        plot_volcano_df(
             data=deg[(deg['niche'] == ctype) & (deg['contrast'] == contrast)],
             x='log2FoldChange',
             y='pvalue',
-            top=10,
+            top=top,
             sign_thr=0.05,
             lFCs_thr=1,
             ax=ax,
